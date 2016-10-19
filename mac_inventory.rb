233278b2614bfc0988925bebd4d3ca897ca65a12
Brandon Kurtz <kurtz.brandon@gmail.com>
2016-10-18 22:40:53 -0400

    reorganize again to make rubocop happy and to make the code more readable

--- mac_inventory.rb
+++ mac_inventory.rb
@@ -1,6 +1,7 @@
 require 'plist'
 
-# stuff
+# Inventory Object. <instance>.gather is a hash of Inventory
+#   information about the Mac this script is run on
 class Inventory
   def parse(datatype: 'SPStorageDataType')
     result = `system_profiler -xml #{datatype}`
@@ -22,22 +23,39 @@ class Inventory
 
   def gather
     @hash['computer_name'] = `scutil --get ComputerName`.strip
+    # gets machine_model, screen_size, serial_number, and physical_memory in the hash
+    hardware_facts
+    # gets cpu_type and processor_speed in the hash
+    processor_facts
+    # gets os_version into hash
+    software_facts
+    # gets each interface and corresponding MAC address in the hash
+    mac_addresses
+    # gets volume_name into hash
+    storage_facts
+  end
+
+  def hardware_facts
     @hash['machine_model'] = @xml_data['SPHardwareDataType']['_items'][0]['machine_model']
     @hash['screen_size'] = `python screen_size.py`.strip
     @hash['serial_number'] = @xml_data['SPHardwareDataType']['_items'][0]['serial_number']
-
     @hash['physical_memory'] =
       @xml_data['SPHardwareDataType']['_items'][0]['physical_memory']
+  end
 
+  def processor_facts
     @hash['processor'] = {
       'cpu_type' => @xml_data['SPHardwareDataType']['_items'][0]['cpu_type'],
       'processor_speed' =>
         @xml_data['SPHardwareDataType']['_items'][0]['current_processor_speed']
     }
+  end
 
+  def software_facts
     @hash['os_version'] = @xml_data['SPSoftwareDataType']['_items'][0]['os_version']
-    @hash['mac_addresses'] = mac_addresses
+  end
 
+  def storage_facts
     @hash['volume_name'] = @xml_data['SPStorageDataType']['_items'][0]['_name']
   end
 
@@ -46,18 +64,19 @@ class Inventory
     @xml_data['SPNetworkDataType']['_items'].each do |interface|
       begin
         interfaces[interface['_name']] = interface['Ethernet']['MAC Address']
+      # if the interface does not have a MAC address
       rescue
-        # if the interface does not have a MAC address
-        # skips interfaces that don't have a MAC address if all_interfaces = FALSE
+        # skips empty interfaces if all_interfaces = FALSE
         interfaces[interface['_name']] = 'empty' if all_interfaces
         next
       end
     end
     # returns hash of interfaces by name with val of MAC Address, if it exists
-    interfaces
+    @hash['mac_addresses'] = interfaces
   end
 
   def show
+    # returns hash
     @hash
   end
 end
